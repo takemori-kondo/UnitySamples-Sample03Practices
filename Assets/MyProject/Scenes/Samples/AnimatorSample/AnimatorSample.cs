645eb91bd8213e3cf4662b7422d0133e66e654dc
HOME-20230701\Ether <etherealgarden@msn.com>
2024-12-01 18:16:47 +0900

    AsyncUtil追加

---
+++ Assets/MyProject/Scenes/Samples/AnimatorSample/AnimatorSample.cs
@@ -0,0 +1,29 @@
+﻿using Assets.UnlimitedFairytales.UnityUtils;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatorSample : MonoBehaviour
+{
+    const string SHOWING = "Showing";
+    const string SHOWN = "Shown";
+    const string CLOSING = "Closing";
+    const string CLOSED = "Closed";
+
+    [SerializeField] Button _button;
+    [SerializeField] Animator _animator;
+
+    bool _isClosed = true;
+
+    void Start()
+    {
+        _button.onClick.AddListener(UniTask.UnityAction(async () =>
+        {
+            var nextStateName = _isClosed ? SHOWING : CLOSING;
+            _isClosed = !_isClosed;
+            _animator.SetTrigger(nextStateName);
+            await AsyncUtil.DelayForAnimation(_animator, nextStateName);
+            Debug.Log($"{nextStateName} complete");
+        }));
+    }
+}
---
+++ Assets/UnlimitedFairytales/UnityUtils/AsyncUtil.cs
@@ -0,0 +1,27 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+namespace Assets.UnlimitedFairytales.UnityUtils
+{
+    public static class AsyncUtil
+    {
+        public static async UniTask DelayForAnimation(Animator animator, string delayStateName, bool isCompletionOnUnexpectedNextState = true, CancellationToken cancellationToken = default, int layerIndex = 0)
+        {
+            if (animator == null) return;
+            await UniTask.NextFrame(cancellationToken); // NOTE: SetTrigger()後にStateが変わるのは Internal animation update後。そのため1フレーム待つ
+            while (true)
+            {
+                var current = animator.GetCurrentAnimatorStateInfo(layerIndex);
+                var next = animator.GetNextAnimatorStateInfo(layerIndex);
+                var isDelayState = current.IsName(delayStateName) || next.IsName(delayStateName);
+                var isUnexpectedNextState = next.shortNameHash != 0 && !next.IsName(delayStateName);
+                if (!isDelayState || (isCompletionOnUnexpectedNextState && isUnexpectedNextState))
+                {
+                    break;
+                }
+                await UniTask.NextFrame(cancellationToken);
+            }
+        }
+    }
+}
