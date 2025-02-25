cdf7d5fa71bb2443995a07efdb1986b43b138b73
HOME-20230701\Ether <etherealgarden@msn.com>
2025-02-25 23:30:48 +0900

    TMPUtil追加

---
+++ Assets/MyProject/Scenes/Samples/TMPUtilSample/TMPUtilSample.cs
@@ -0,0 +1,26 @@
+﻿using Assets.UnlimitedFairytales.UnityUtils.UI;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TMPUtilSample : MonoBehaviour
+{
+    [SerializeField] Button _button;
+    [SerializeField] TMP_Text _tmpText;
+    [SerializeField] ScrollRect _scrollRect;
+
+    void Start()
+    {
+        _button.onClick.AddListener(UniTask.UnityAction(async () =>
+        {
+            _tmpText.text = "";
+            await _tmpText.TypeWriterEffectAsync(@"O, that this too too solid flesh would melt,
+Thaw, and resolve itself into a dew!
+Or that the Everlasting had not fix'd
+His canon 'gainst self-slaughter! O God! God!
+How weary, stale, flat, and profitable
+Seem to me all the uses of this world!", TMPUtil.ONE_FRAME * 2, default, _scrollRect);
+        }));
+    }
+}
---
+++ Assets/UnlimitedFairytales/UnityUtils/UI/TMPUtil.cs
@@ -0,0 +1,44 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.UnlimitedFairytales.UnityUtils.UI
+{
+    public static class TMPUtil
+    {
+        public const float ONE_FRAME = 0.0166f;
+
+        /// <summary>
+        /// 簡易的なタイプライターエフェクト。より実用的な機能が欲しい場合は、DOTween Proなどを使用してください
+        /// </summary>
+        /// <param name="tmp"></param>
+        /// <param name="text"></param>
+        /// <param name="wait_sec"></param>
+        /// <param name="ct"></param>
+        /// <param name="scrollRect"></param>
+        /// <param name="scrollDuration_sec"></param>
+        /// <returns></returns>
+        public static async UniTask TypeWriterEffectAsync(this TMP_Text tmp, string text, float wait_sec = ONE_FRAME * 2, CancellationToken ct = default, ScrollRect scrollRect = null, float scrollDuration_sec = 0.3f)
+        {
+            var waitCounter = 0.0f;
+            foreach (var c in text)
+            {
+                tmp.text += c.ToString();
+                waitCounter += wait_sec;
+                if (ONE_FRAME <= waitCounter)
+                {
+                    await UniTask.WaitForSeconds(wait_sec);
+                    waitCounter = 0.0f;
+                }
+                if (ct.IsCancellationRequested) return;
+                if (scrollRect != null && 0.001f < scrollRect.verticalNormalizedPosition && !DOTween.IsTweening(scrollRect))
+                {
+                    await scrollRect.DOVerticalNormalizedPos(0.0f, scrollDuration_sec);
+                }
+            }
+        }
+    }
+}
