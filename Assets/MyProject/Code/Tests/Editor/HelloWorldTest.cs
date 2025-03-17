a49d92c48a577de369155a8ea5bac4a64a5c5eee
HOME-20230701\Ether <etherealgarden@msn.com>
2025-03-17 15:38:02 +0900

    EditMode Testコードサンプル追加

---
+++ Assets/MyProject/Code/Tests/Editor/HelloWorldTest.cs
@@ -0,0 +1,18 @@
+﻿using Assets.MyProject.Code.Scripts;
+using NUnit.Framework;
+
+public class HelloWorldTest
+{
+    [Test]
+    public void HelloWorldTestSimplePasses()
+    {
+        // Arrange
+        var obj = new HelloWorld();
+
+        // Act
+        var result = obj.Hello();
+
+        // Assert
+        Assert.AreEqual("hello, world", result);
+    }
+}
